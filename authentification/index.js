4d9e59e66098a5d47c8b0a87ad05743109e63636
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-08-04 17:17:50 +0500

    change

--- authentification/index.js
+++ authentification/index.js
@@ -5,6 +5,7 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 5555;
 const server = app.listen(port, () => {
