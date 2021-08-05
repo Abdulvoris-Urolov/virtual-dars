b9afd8f2fa1f1781763af700d8ef1d1194fb39a0
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-08-05 13:19:08 +0500

    changed

--- authentification/startup/db.js
+++ authentification/startup/db.js
@@ -6,6 +6,10 @@ module.exports = function () {
     mongoose.connect(config.get('db'), { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     winston.debug('MongoDBga ulanish hosil qilindi...');
+    console.log('MongoDBga ulanish hosil qilindi...');
+  })
+  .catch((error)=>{
+    console.error('mongoga ulanmadi', error);
   });
 mongoose.set('useCreateIndex', true);
 }
